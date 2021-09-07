21213f19b655cb75f28b6380287f743497119f18
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-07 21:41:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# task1_NTI_NodeJs
+ 
---
+++ app.js
@@ -0,0 +1,5 @@
+const tasks = require('./myModules/tasks')
+    // tasks.addTask({ "title": "task 3", "content": "c 3", "dueDate": "22/2/2000", "type": "t 3" })
+    // tasks.showAll();
+    // tasks.searchTask(433841181197)
+tasks.deleteTask(425216940214);
