e6ecd311e1c3c5462400bb59e4b5506f0549f8f1
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-15 11:27:55 +0200

    message

---
+++ task4/index.js
@@ -0,0 +1,30 @@
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+//const ObjectID =mongodb.ObjectID
+const { MongoClient, ObjectId } = require('mongodb')
+
+const dbURL = "mongodb://127.0.0.1:27017"
+const dbName = "g10DB"
+
+MongoClient.connect(dbURL, {}, (err, client) => {
+    if (err) return console.log('unable to connect to db')
+
+    const db = client.db(dbName)
+
+    //     db.collection('students').insertMany([{ name: "marwa", age: 36 }, { name: "laila", age: 45 }, { name: "ahmed", age: 57 }],
+    //         (error, result) => {
+    //             if (error) return console.log("error inserting data")
+    //             console.log('inserted success')
+    //             console.log(result.insertedCount)
+    //         })
+
+
+
+    // db.collection('students').find().toArray((err, res) => {
+    //     console.log(res)
+    // })
+
+    db.collection('students').findOne({ _id: new ObjectId("6141b39b2aed6c601cdf7186") },
+        ((err, res) => { console.log(res) })
+    )
+})
