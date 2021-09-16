c8d153cd3efd58c1161b35867f46b1ac53f18285
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-16 02:22:52 +0200

    mongodbapp

--- customerAppWthMongodb/src/app.js
+++ customerAppWthMongodb/src/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 
-const app = express()
 
+const app = express()
 const userRoutes = require('../routes/user.routes')
 
 app.set('view engine', 'hbs')
@@ -13,8 +13,10 @@ hbs.registerPartials(path.join(__dirname, "../frontend/layouts"))
 
 app.use(express.static(path.join(__dirname, "../public")))
 
-app.use(express.urlencoded()) // use form data
+app.use(express.urlencoded({ extended: true })) // use form data
+
 
 app.use(userRoutes)
+
 
 module.exports = app
