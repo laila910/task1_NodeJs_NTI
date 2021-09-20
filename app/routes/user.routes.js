51ab66426fc22cea1c831210d4362e9e2cf880ea
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-09-20 11:15:30 +0200

    middlewar

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/user.model')
-
-// - register
+const auth = require('../middleware/auth')
+    // - register
 router.post('/register', async(req, res) => {
         try {
             const userData = new User(req.body)
@@ -43,12 +43,18 @@ router.post('/addAddr/:id', async(req, res) => {
 router.post('/login', async(req, res) => {
         try {
             let user = await User.loginUser(req.body.email, req.body.password)
-            res.status(200).send({ apiStatus: true, data: user, message: "logged in " })
+            const token = await user.generateToken()
+            res.status(200).send({ apiStatus: true, data: { user, token }, message: "logged in" })
         } catch (e) {
             res.status(500).send({ apiStatus: false, data: e.message, message: "cann't login " })
         }
     })
     // - logout
+    //profile
+
+router.post('/me', auth, async(req, res) => {
+        res.send(req.user)
+    })
     // - profile
     // - edit profile
     // - deactivate 
